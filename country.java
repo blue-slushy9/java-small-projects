79962e118fdc3e46f43b609614e4b1e9d007ff27
blue-slushy9 <ra.cruz971@gmail.com>
2024-11-19 15:22:10 -0800

    Might have finished refactoring country.java

--- country.java
+++ country.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
+
 // 'class Program' is a common alternative to 'class Main'
 class Program {
 	
 	public static void main(String[] args) {
-	    // Create new instance of Country object called 'c'
+	    // Scanner block
+        Scanner read = New Scanner();
+		String name = read.nextLine();
+		String capital = read.nextLine();
+
+		// Create instance of Country object and call Setter methods
 	    Country c = new Country();
-	    // Set object attributes
-	    c.name = "France";
-	    c.capital = "Paris";
-		// Print fields and object attributes
-	    System.out.println("Country: " + c.name);
-	    System.out.println("Capital: " + c.capital);
+		c.name = setName(name);
+	    c.capital = setCapital(capital);
+
+		// Print fields and object attributes using Getter method
+	    System.out.println("Country: " + getName());
+	    System.out.println("Capital: " + getCapital());
     }
 }
 
@@ -19,4 +26,22 @@ class Country {
     // Private access modifier restricts access to only this class
     private String name;
     private String capital;
+
+	// Setter method for name
+	public void setName(String name) {
+        this.name = name;
+	}
+	// Getter method for name
+	public String getName() {
+		return name;
+	}
+
+	// Setter method for capital
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+	// Getter method for capital
+	public String getCapital() {
+		return capital;
+	}
 }
