b393ac76779fc7dd85260e7376d1922eddcd09a5
blue-slushy9 <ra.cruz971@gmail.com>
2024-12-09 17:26:08 -0800

    Made a few more changes to Pupil.java

--- pupil/Pupil.java
+++ pupil/Pupil.java
@@ -12,14 +12,15 @@ public class Pupil {
 	    
         // Create instance of object and call its methods
 	    PupilDetails pupil =  new PupilDetails();
-            // Call setter method
-            pupil.setAge(age);
-            // Print result and user input using getter method
-            System.out.println(pupil.getResult());
+        // Call setter method
+        pupil.setAge(age);
+        // Print result and user input using getter method
+        System.out.println(pupil.getResult());
 	}
 }
 
 class PupilDetails {
+
     // Default value for integers is 0
     private int age;
     // Default value for strings is null
@@ -27,13 +28,14 @@ class PupilDetails {
     
     // Define conditional setter method
     public void setAge(int a) {
+        // Set age according to argument
         this.age = a;
-        // If age is greater than 6
         if (age > 6) {
             this.result = ("Welcome, %d is old enough to study.".formatted(age));
         // Not old enough
         } else if (age <= 6 && age >= 0) {
             this.result = ("Sorry, %d isn't old enough to study.".formatted(age));
+        // Invalid age
         } else {
             this.result = ("%d is not a valid age, please enter a real age.".formatted(age));
         }
