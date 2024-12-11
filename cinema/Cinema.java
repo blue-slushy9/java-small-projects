9695e71bc894577c3aff7fcf452898205d9c587f
slushy <107578323+blue-slushy9@users.noreply.github.com>
2024-12-11 02:20:40 +0000

    Almost finished with Cinema, it works but I might want to add a few more features.

--- cinema/Cinema.java
+++ cinema/Cinema.java
@@ -1,21 +1,32 @@
 // Used to take user input via terminal
 import java.util.Scanner;
 
-public class Main {
+public class Cinema {
 
     public static void main(String[] args) {
         // Create instance of a Scanner object called 'read'
         Scanner read = new Scanner(System.in);
+        System.out.print("Please enter the name of the movie: ");
         String movie = read.nextLine();
-        int row = read.nextInt();
+        System.out.print("Please enter your row letter: ");
+        // We need to use charAt(0) because next() is for strings
+        char row = read.next().charAt(0);
+        System.out.print("Please enter your seat number: ");
         int seat = read.nextInt();
+        // Close scanner to preserve system resources
+        read.close();
+        // Blank line for legibility
+        System.out.println();
         
         // Create instance of a Ticket object called 'ticket'
         Ticket ticket = new Ticket(movie, row, seat);
-        // Print out ticket details, including user input
-        System.out.println("Movie: " + ticket.getMovie());
-        System.out.println("Row: " + ticket.getRow());
-        System.out.println("Seat: " + ticket.getSeat());
+        // Define string that will confirm user's ticket details
+        String confirmation = ("Okay, so your ticket says you are going to see " +
+                              ticket.getMovie() + ", and that you'll be sitting " +
+                              "in row " + ticket.getRow() + " in seat number " + 
+                              ticket.getSeat() + ". Is this correct?"
+        
+        System.out.println(confirmation);
     }
 }
 
@@ -23,12 +34,12 @@ class Ticket {
     
     // Declare class variables
     private String movie;
-    private int row;
+    private char row;
     private int seat;
 
     // Constructor is used to create an instance of the Ticket object,
     // in this case a parameterized constructor with specific values
-    public Ticket(String movie, int row, int seat) {
+    public Ticket(String movie, char row, int seat) {
         // 'this' refers to variables specific to the object instance
         this.movie = movie;
         this.row = row;
@@ -39,7 +50,7 @@ class Ticket {
     public String getMovie() {
         return movie;
     }
-    public int getRow() {
+    public char getRow() {
         return row;
     }
     public int getSeat() {
